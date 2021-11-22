a05b633f9cc859e160a20587653e06d4a02a559e
Mohan kumar A <mohancode30@gmail.com>
2021-11-22 18:45:52 +0530

    chaged greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send({hi: 'there'});
+    res.send({Bye: 'buddy'});
 })
 
 const PORT = process.env.PORT || 5000; // for dev there is no env variable so we assign the 5000 bydefault
